01cf7609b570577a2d6c52e173bd35aadea503e6
noydch <noydch2003@gmail.com>
2025-07-12 16:53:18 +0700

    12-7-2025(2)

--- src/api/apiPath.js
+++ src/api/apiPath.js
@@ -1,5 +1,5 @@
-const baseUrl = 'http://localhost:5050/api';
-// const baseUrl = 'https://safezone-backend-hg65.onrender.com/api';
+// const baseUrl = 'http://localhost:5050/api';
+const baseUrl = 'https://safezone-backend-hg65.onrender.com/api';
 
 
 export default class ApiPath {
